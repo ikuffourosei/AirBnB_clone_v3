010fd2fef0eb8753d9375f9917333e8588ebd07e
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-03-31 22:58:06 +0000

    page not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Starting a Flask Application"""
-from flask import Flask
+from flask import Flask, Response, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -14,6 +14,12 @@ app.register_blueprint(app_views)
 def teardown_session(exception):
     """close database session"""
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_foud(error) -> Response:
+    """Page not found"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
