7536c23db50f85ea4a1b49cf7ef6044b3eb271b7
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-03-31 21:19:26 +0000

    setting blueprint to return a JSON response

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,4 +7,4 @@ from flask import jsonify
 @app_views.route('/status', methods=['GET'])
 def index():
     '''returns a JSON: "status": "OK" '''
-    return jsonify({'status': 'OK'})
+    return jsonify({'status': 'OK'}), 200
