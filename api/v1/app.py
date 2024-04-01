17f53d519d72ff8d4e7ec8cf7189d743a6380bc2
amofasandra7@gmail.com <amofasandra7@gmail.com>
2024-04-01 20:52:51 +0000

    HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,7 +9,7 @@ import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-CORS(app, resources="/api/*", origin="0.0.0.0")
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
