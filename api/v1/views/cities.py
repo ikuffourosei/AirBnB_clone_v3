632b99ce379d74051e941d330198e6971c92ca67
amofasandra7@gmail.com <amofasandra7@gmail.com>
2024-04-01 23:09:52 +0000

    debugged

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -5,11 +5,9 @@ Flask route that returns json status response
 from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models import storage, CNC
-from flasgger.utils import swag_from
 
 
 @app_views.route('/states/<state_id>/cities', methods=['GET', 'POST'])
-@swag_from('swagger_yaml/cities_by_state.yml', methods=['GET', 'POST'])
 def cities_per_state(state_id=None):
     """
         cities route to handle http method for requested cities by state
