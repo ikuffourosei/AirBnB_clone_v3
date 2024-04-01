5eed6f462826f1439d87ac81a1dd56f0dc2ca7d1
amofasandra7@gmail.com <amofasandra7@gmail.com>
2024-04-01 22:26:57 +0000

    debugged

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,24 +2,14 @@
 """Blueprint of views"""
 from api.v1.views import app_views
 from flask import jsonify
-import models
+from models import storage
 from models.amenity import Amenity
-from models.base_model import BaseModel
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.user import User
 from models.state import State
 
-
-classes = {'Amenity': Amenity,
-           'Review': Review,
-           'City': City,
-           'State': State,
-           'Place': Place,
-           'User': User,
-           'BaseModel': BaseModel
-           }
 
 
 @app_views.route('/status', methods=['GET'])
@@ -28,10 +18,16 @@ def index():
     return jsonify({'status': 'OK'})
 
 
-@app_views.route('/stats', methods=['GET'])
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def get_status():
     """an endpoint that retrieves the number of each objects by type:"""
-    data = {}
-    for key, val in classes.items():
-        data[key] = models.storage.count(val)
+    data = {
+            "amenities": storage.count("Amenity"),
+            "cities": storage.count("City"),
+            "places": storage.count("Place"),
+            "reviews": storage.count("Review"),
+            "states": storage.count("State"),
+            "users": storage.count("User")
+    }
+
     return jsonify(data)
